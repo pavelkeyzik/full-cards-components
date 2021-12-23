8165610caaadfc7054b225d2b986c2c7a471d8b9
Pavel Keyzik <pavel.keyzik@gmail.com>
2021-12-23 23:04:17 +0300

    feat: add storybook configuration
    - Installed storybook-dark-mode to be able change theme of Storybook
    - Installed @storybook/addon-a11y to see A11Y issues while
      building something
    - Installed @storybook/addong-storysource to see source code of story
      inside storybook preview file

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import { themes } from '@storybook/theming';
 import '../src/styles/index.scss';
 
 export const parameters = {
@@ -7,6 +8,10 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  darkMode: {
+    dark: themes.dark,
+    light: themes.normal,
   },
 };
 
