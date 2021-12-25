51d73e3691393cc97356a48cb91d035c8b907376
Pavel Keyzik <pavel.keyzik@gmail.com>
2021-12-25 20:20:05 +0300

    feat: add Avatar component

---
+++ src/stories/Avatar.stories.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+export function Avatar() {
+  return (
+    <div className="story-grid">
+      <div className="story-grid-row">
+        <div className="avatar avatar-xs">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+        <div className="avatar avatar-sm">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+        <div className="avatar avatar-md">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+        <div className="avatar avatar-lg">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+        <div className="avatar avatar-xl">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+      </div>
+      <div className="story-grid-row">
+        <div className="avatar avatar-outline avatar-xs">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+        <div className="avatar avatar-outline avatar-sm">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+        <div className="avatar avatar-outline avatar-md">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+        <div className="avatar avatar-outline avatar-lg">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+        <div className="avatar avatar-outline avatar-xl">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+      </div>
+      <div className="story-grid-row">
+        <div className="avatar avatar-online avatar-xs">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+        <div className="avatar avatar-online avatar-sm">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+        <div className="avatar avatar-online avatar-md">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+        <div className="avatar avatar-online avatar-lg">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+        <div className="avatar avatar-online avatar-xl">
+          <img src="https://picsum.photos/100" alt="Avatar" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default {
+  title: 'Example/Avatar',
+};
