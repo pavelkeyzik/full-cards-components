4a19b3f2d371dab37283da2cbd91939d5221d756
Pavel Keyzik <pavel.keyzik@gmail.com>
2021-12-27 23:38:09 +0300

    feat: add Card component

---
+++ src/stories/Cards.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ReactComponent as CarbonMenuIcon } from '../assets/icons/carbon-menu.svg';
+
+export function Cards() {
+  return (
+    <div className="story-grid-no-background">
+      <div className="card">
+        <div className="card-header">
+          <div className="media">
+            <div className="avatar">
+              <img src="https://picsum.photos/100" alt="Avatar" />
+            </div>
+            <div className="media-content">
+              <h2 className="media-title">Frank Esteban</h2>
+              <span className="media-subtitle">Web Development</span>
+            </div>
+            <i className="icon">
+              <CarbonMenuIcon />
+            </i>
+          </div>
+        </div>
+        <div className="card-image">
+          <img src="https://picsum.photos/360/184" alt="Something random" />
+        </div>
+        <div className="card-body">This is a basic card</div>
+      </div>
+    </div>
+  );
+}
+
+export default {
+  title: 'Example/Cards',
+};
