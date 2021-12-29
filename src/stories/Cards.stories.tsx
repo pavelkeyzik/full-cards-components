6687fd4634f0d427c5dc467362320310c44f258b
Pavel Keyzik <pavel.keyzik@gmail.com>
2021-12-29 09:39:12 +0300

    feat: add third variant of card

--- src/stories/Cards.stories.tsx
+++ src/stories/Cards.stories.tsx
@@ -41,6 +41,25 @@ export function Cards() {
         </div>
         <div className="card-footer">3 September 2019</div>
       </div>
+      <div className="card">
+        <div className="card-image">
+          <img src="https://picsum.photos/360/184" alt="Something random" />
+        </div>
+        <div className="card-header">
+          <div className="media">
+            <div className="avatar avatar-outline">
+              <img src="https://picsum.photos/100" alt="Avatar" />
+            </div>
+            <div className="media-content">
+              <h2 className="media-title">Frank Esteban</h2>
+              <span className="media-subtitle">Web Development</span>
+            </div>
+            <i className="icon">
+              <CarbonMenuIcon />
+            </i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
