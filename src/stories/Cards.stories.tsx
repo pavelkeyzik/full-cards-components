bbbb099693a285a4d8b9df40ed293faea704987c
Pavel Keyzik <pavel.keyzik@gmail.com>
2021-12-29 09:45:10 +0300

    feat: add more cards

--- src/stories/Cards.stories.tsx
+++ src/stories/Cards.stories.tsx
@@ -51,7 +51,64 @@ export function Cards() {
               <img src="https://picsum.photos/100" alt="Avatar" />
             </div>
             <div className="media-content">
-              <h2 className="media-title">Frank Esteban</h2>
+              <h2 className="media-title">Wade Warren</h2>
+              <span className="media-subtitle">Software Tester</span>
+            </div>
+            <i className="icon">
+              <CarbonMenuIcon />
+            </i>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-header">
+          <div className="media">
+            <div className="avatar">
+              <img src="https://picsum.photos/100" alt="Avatar" />
+            </div>
+            <div className="media-content">
+              <h2 className="media-title">Savannah Nguyen</h2>
+              <span className="media-subtitle">Web Development</span>
+            </div>
+          </div>
+        </div>
+        <div className="card-body">
+          <p>Minim dolor in amet nulla laboris</p>
+        </div>
+        <div className="card-image">
+          <img src="https://picsum.photos/360/184" alt="Something random" />
+        </div>
+        <div className="card-footer">October 14</div>
+      </div>
+      <div className="card">
+        <div className="card-header">
+          <div className="media">
+            <div className="avatar">
+              <img src="https://picsum.photos/100" alt="Avatar" />
+            </div>
+            <div className="media-content">
+              <h2 className="media-title">Devon Lane</h2>
+              <span className="media-subtitle">Web Development</span>
+            </div>
+            <button className="btn btn-outline">Button</button>
+          </div>
+        </div>
+        <div className="card-image">
+          <img src="https://picsum.photos/360/184" alt="Something random" />
+        </div>
+        <div className="card-body">
+          <p>Minim dolor in amet nulla laboris</p>
+        </div>
+        <div className="card-footer">23 April 2021</div>
+      </div>
+      <div className="card">
+        <div className="card-header">
+          <div className="media">
+            <div className="avatar">
+              <img src="https://picsum.photos/100" alt="Avatar" />
+            </div>
+            <div className="media-content">
+              <h2 className="media-title">Annette Black</h2>
               <span className="media-subtitle">Web Development</span>
             </div>
             <i className="icon">
@@ -59,6 +116,13 @@ export function Cards() {
             </i>
           </div>
         </div>
+        <div className="card-image">
+          <img src="https://picsum.photos/360/184" alt="Something random" />
+        </div>
+        <div className="card-body">
+          <p>Minim dolor in amet nulla laboris</p>
+        </div>
+        <div className="card-footer">January 07</div>
       </div>
     </div>
   );
